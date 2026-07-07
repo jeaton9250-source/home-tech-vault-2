4f89b445f5d0db567c334f9056fb6e8dc630f89e
Jeaton45 <jjeaton@my.waketech.edu>
2026-07-07 01:39:51 -0400

    Complete Network Center MVP

---
+++ components/PageHeader.tsx
@@ -0,0 +1,22 @@
+type PageHeaderProps = {
+  title: string;
+  description: string;
+  action?: React.ReactNode;
+};
+
+export default function PageHeader({
+  title,
+  description,
+  action,
+}: PageHeaderProps) {
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <h1 className="text-4xl font-bold text-blue-950">{title}</h1>
+        <p className="text-gray-600 mt-2">{description}</p>
+      </div>
+
+      {action && <div>{action}</div>}
+    </div>
+  );
+}
