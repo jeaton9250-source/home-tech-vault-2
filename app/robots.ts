2759554c06d0a24afd36b039b61713b9a1c72b44
jeaton9250-source <jeaton9250@gmail.com>
2026-08-16 19:49:31 -0400

    Fix robots rule for home SEO pages

--- app/robots.ts
+++ app/robots.ts
@@ -19,7 +19,7 @@ export default function robots(): MetadataRoute.Robots {
         "/devices",
         "/documents",
         "/network",
-        "/home",
+        "/home/",
         "/family",
         "/settings",
         "/account",
